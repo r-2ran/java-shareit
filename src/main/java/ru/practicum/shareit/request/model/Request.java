d477f26a4d7184937a430634970505859d10d8d6
2ran <brakemire@yandex.ru>
2023-09-16 02:15:37 +0300

    fix schema.sql

--- src/main/java/ru/practicum/shareit/request/model/Request.java
+++ src/main/java/ru/practicum/shareit/request/model/Request.java
@@ -24,7 +24,7 @@ public class Request {
     @NotBlank
     String description;
     @ManyToOne
-    @JoinColumn(name = "requestor", referencedColumnName = "id")
+    @JoinColumn(name = "requestor_id", referencedColumnName = "id")
     User requestor;
     LocalDateTime created;
 }
